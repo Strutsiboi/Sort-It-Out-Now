0e4886c0506f3e80cf1d1bf4c8553361d3f84ba7
Strutsiboi <melker.johansson@elev.ga.lbs.se>
2023-01-13 15:21:24 +0100

    sss

--- Assets/Scripts/TrashController.cs
+++ Assets/Scripts/TrashController.cs
@@ -11,15 +11,17 @@ public class TrashController : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Trash" && !attached)
+        if (collision.gameObject.tag == "Plast" && !attached)
         {
             objectToAttach.transform.parent = player.transform;
             objectToAttach.transform.position = player.transform.position;
             attached = true;
+            print("TAR UPP SOPOR");
         }
 
         if (collision.gameObject.tag == "Bin" && attached)
         {
+            print("SLÄNG");
             Destroy(objectToAttach);
             attached = false;
         }
