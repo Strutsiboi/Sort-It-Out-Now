117e91ba0c0483e5dd8e02432f581f70cf82987e
MikhaelG <mikhael.gerber@elev.ga.lbs.se>
2023-01-16 11:35:47 +0100

    Health system (inte hel)

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    void Update()
+    {
+
+        if(health > numOfHearts) //ser till så att spelaren kan inte ha mer hjärtan än max
+        {
+            health = numOfHearts;
+        }
+
+        for (int i = 0; i < hearts.Length; i++) //forloop
+        {
+            if(i < health)
+            {
+                hearts[i].sprite = fullHeart; //ifall i är mindre än hjärtan, så finns de
+                
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart; //ifall i är större än hjärtan, är de tomma
+            }
+
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        } 
+        
+
+    }
+}
