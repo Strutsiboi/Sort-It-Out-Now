a2d86d9f2fd7640ccb83ab52d895d33beeb8a728
MikhaelG <mikhael.gerber@elev.ga.lbs.se>
2023-01-16 13:10:39 +0100

    score

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -5,21 +5,29 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
+    public static ScoreManager instance;
+
     public Text scoreText;
     public Text highscoreText;
 
     int score = 0;
     int highscore = 0;
-    
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     void Start()
     {
-        
+        scoreText.text = score.ToString() + "POINTS";
+        highscoreText.text = "HIGHSCORE:" + highscore.ToString();
     }
 
  
-    void Update()
-    {
-        
-    }
+   public void AddPoint()
+   {
+        score += 1;
+        scoreText.text = score.ToString() + "POINTS";
+   }
 }
