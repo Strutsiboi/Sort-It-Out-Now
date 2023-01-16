73490956e3e9f77b1af148085ac0ebd23d6b0a72
MikhaelG <mikhael.gerber@elev.ga.lbs.se>
2023-01-16 12:58:30 +0100

    påbörjan av score

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    public Text highscoreText;
+
+    int score = 0;
+    int highscore = 0;
+    
+
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+        
+    }
+}
