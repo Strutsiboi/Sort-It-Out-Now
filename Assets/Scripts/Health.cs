c2d4e4d52ed1ed90da9a4fb9376719593924d9a3
MikhaelG <mikhael.gerber@elev.ga.lbs.se>
2023-01-19 10:34:14 +0100

    iugh

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -40,12 +40,43 @@ public class Health : MonoBehaviour
             {
                 hearts[i].enabled = false;
             }
-            void OnCollisionEnter2D(Collision2D collision)
-            {
-                hearts[i].sprite = emptyHeart;
-            }
+
+            
         }
 
         
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Plast" || collision.gameObject.tag == "Metall" || collision.gameObject.tag == "Glas" || collision.gameObject.tag == "Kartong")
+        {
+            for (int i = 0; i < hearts.Length; i++) //forloop
+            {
+                if (i < health)
+                {
+                    hearts[i].sprite = fullHeart; //ifall i är mindre än hjärtan, så finns de
+
+                }
+                else
+                {
+                    hearts[i].sprite = emptyHeart; //ifall i är större än hjärtan, är de tomma
+                }
+
+                if (i < numOfHearts)
+                {
+                    hearts[i].enabled = true;
+                }
+                else
+                {
+                    hearts[i].enabled = false;
+                }
+
+
+            }
+
+        }
+
+    }
+
 }
