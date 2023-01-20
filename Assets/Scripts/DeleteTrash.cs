d2b83ea250e9f68ba054befc8cd54ae142789935
Strutsiboi <melker.johansson@elev.ga.lbs.se>
2023-01-20 10:27:04 +0100

    sss

--- Assets/Scripts/DeleteTrash.cs
+++ Assets/Scripts/DeleteTrash.cs
@@ -6,6 +6,7 @@ public class DeleteTrash : MonoBehaviour
 {
     public void OnCollisionEnter2D(Collision2D collision)
     {
+        print("kollision");
         if (collision.gameObject.tag == "VeryBadBin")
         {
             print("COLLIDAR MED OBJEKT");
